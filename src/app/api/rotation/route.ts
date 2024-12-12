39bed54791c4a40f3b3fbe5f3c0787b7b88b1635
leekyoungmin <zzxx66052@gmail.com>
2024-12-12 13:12:15 +0900

    add: route-api설정

--- src/app/api/rotation/route.ts
+++ src/app/api/rotation/route.ts
@@ -1,17 +1,36 @@
-export async function GET() {
-  const apiKey = process.env.RIOT_API_KEY;
-  if (!apiKey) {
-    throw new Error("not definded apiKey");
-  }
-  console.log(apiKey);
+import { NextResponse } from "next/server";
 
-  const apiurl =
+export async function GET(request: Request) {
+  //apiUrl
+  const apiUrl =
     "https://kr.api.riotgames.com/lol/platform/v3/champion-rotations";
 
-  const response = await fetch(apiurl, {
-    headers: {
-      "X-Riot-Token": apiKey,
-    },
-  });
-  return response;
+  //apiKey
+  const apiKey = process.env.RIOT_API_KEY;
+  if (!apiKey) {
+    return NextResponse.json({
+      error: "not definded apiKey",
+    });
+  }
+
+  try {
+    const response = await fetch(apiUrl, {
+      headers: {
+        "X-Riot-Token": apiKey,
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error(
+        `Riot API Error: ${response.status} - ${response.statusText}`
+      );
+    }
+
+    const data = await response.json();
+    return NextResponse.json(data);
+  } catch (error: any) {
+    return NextResponse.json({
+      error: error.message || "반응이 없어서 에러가 발생했습니다.",
+    });
+  }
 }
