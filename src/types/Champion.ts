c0775e7692e1378681c60f68e5fda414c6b4fd26
leekyoungmin <zzxx66052@gmail.com>
2024-12-12 13:47:54 +0900

    create: champion ts file

---
+++ src/types/Champion.ts
@@ -0,0 +1,38 @@
+type champion = {
+  id: string;
+  key: string;
+  name: string;
+  title: string;
+  image: {
+    full: string;
+    sprite: string;
+    group: string;
+    x: number;
+    y: number;
+    w: number;
+    h: number;
+  };
+};
+
+type championDetail = {
+  id: string;
+  key: string;
+  name: string;
+  title: string;
+  blurb: string;
+  image: {
+    full: string;
+    sprite: string;
+    group: string;
+    x: number;
+    y: number;
+    w: number;
+    h: number;
+  };
+  state: {
+    hp: number; //hp
+    armor: number; //방어력
+    spellblock: number; //마법저항력
+    attackdamage: number; //공격력
+  };
+};
